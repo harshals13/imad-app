c2cb9675596c650b44a6ca63ee6c4c2bcd169ff1
Harsha Suryavamshi <harshasuryavamshi@gmail.com>
2017-08-14 23:07:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,12 +88,13 @@ var htmlTemplate=`
 </html>
 `;
 }
+
 app.get('/', function (req, res) {
-  res.send(createTemplate(articleOne));
+  res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName',function(req,res){
+    res.send(createTemplate(articles[articleName]));
 })
 
 app.get('/article-two',function(req,res){
