b4cf679557d61c59f69d29608fc9f5f46f381034
Harsha Suryavamshi <harshasuryavamshi@gmail.com>
2017-08-21 22:29:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,27 @@ submit.onclick = function(){
     //create request object
     var request = new XMLHttpRequest();
     // Capture the response and store it in a variable
-    re
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status=== 200){
+                //capture a list of names and render is as a list
+                var names = request.responseText;
+                names= JSON.parse(names);
+                var list ='';
+                for(var i=0; i<names.length;i++){
+                    list+= '<li>'+ names[i] + '<li>';
+                }
+                var ul= document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    
+                }
+            }
+        }
+    }
 }
 
 
