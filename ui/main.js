ecf8526c350560a5df10eec90075d56aadde0e1b
Harsha Suryavamshi <harshasuryavamshi@gmail.com>
2017-08-22 00:07:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,69 +1,25 @@
-// counter code
-
-var button = document.getElementById('counter');
-
-button.onclick =  funtion(){
-    
-    // create a request object
-    
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    
-    request.onreadystatechange=function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            //take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    //make the request
-   request.open('GET','http://harshasuryavamshi.imad.hasura-app.io/counter',true);
-   request.send(null);
-}
-
-
-//Submit name
-
-var submit=document.getElementById('submit_btn');
-submit.onclick = function(){
-    
-    //create request object
-    
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
-    
-    request.onreadystatechange = function(){
-        
-        if(request.readyState === XMLHttpRequest.DONE){
-            
-            //take some action
-            
-            if(request.status=== 200){
-                
-                //capture a list of names and render is as a list
-                var names = request.responseText;
-                names= JSON.parse(names);
-                var list ='';
-                for(var i=0; i<names.length;i++){
-                    list+= '<li>'+ names[i] + '<li>';
-                }
-                var ul= document.getElementById('namelist');
-                ul.innerHTML=list;
-            }
-        }
-    };
-     //Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://harshasuryavamshi.imad.hasura-app.io/submit-name?name=' + name,true);
-    request.send(null);
-};
-    
-
-
-
+//counter code
+ var button = document.getElementById('counter');
+ button.onclick = function (){
+     //create a request object
+     
+     var request = XMLHttpRequest();
+     
+     //capture the response and store it in a variable
+     
+     request.onreadystatechange = function () {
+         if(request.readyState===XMLHttpRequest.DONE) {
+             //take some action
+             
+             if(request.status===200) {
+                 var counter = request.responseText;
+                 var span = document.getElementById('count');
+                 span.innerHTML = counter.toString();
+             }
+         }
+         //not done yet
+     };
+     //make the request
+     request.open('GET','http://harshasuryavamshi.imad.hasura-app.io/',true);
+     request.send(null);
+ };
