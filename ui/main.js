0acfbb6a3bb2c27f8d9b6729bce9e66cd8722606
Harsha Suryavamshi <harshasuryavamshi@gmail.com>
2017-08-21 17:03:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
-console.log('Loaded!');
+// counter code
+
+var button = document.getElementById('counter');
+buttin.onclick =  funtion(){
+    // create a request object
+    var request = new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    
+    request.onreadystatechange=function(){
+        if(request.resdyState === XMLHttpRequest.Done){
+            //take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+}
