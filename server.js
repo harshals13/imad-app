c7150c31c0e867c0f79b1b56bde180246da1bb7d
Harsha Suryavamshi <harshasuryavamshi@gmail.com>
2017-08-21 16:52:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
+var names=[];
+app.get('/submit-name',function(req,res){ // /submit.name?name-xxxx
+// Get the name from the request
+var name = req.query.name;
+names.push(name);
+//JSON:Javascript Object Notation
+res.send(JSON.stringify(names));
+
+});
+
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
@@ -108,6 +124,10 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/ui/main.js',function(req,res){
+    res,sendFile(path.join(__dirname,'ui','main.js'));
 });
 
 
