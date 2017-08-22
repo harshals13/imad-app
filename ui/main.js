f0c4564e3610cb1d34a41c742a63c8f7d0fce0dc
Harsha Suryavamshi <harshasuryavamshi@gmail.com>
2017-08-22 08:45:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,10 @@
              //take some action
              
              if(request.status===200) {
-                 var counter = request.responseText;
+                  var counter = request.responseText;
                  var span = document.getElementById('count');
                  span.innerHTML = counter.toString();
+                
              }
          }
          //not done yet
@@ -29,15 +30,28 @@
  var name = nameInput.value;
  var submit = document.getElementById('submit_btn');
  submit.onclick = function(){
-     //make a request to the server and send the name
+    //capture the response and store it in a variable
      
-     //capturea alist of names and render it as a list
-     var names= ['name1','name2','name3'];
-     var list='';
-     for(var i=0; i< names.length; i++){
-         list += '<li>' + names[i] + '</li>';
-     }
+     request.onreadystatechange = function (){
+         if(request.readyState===XMLHttpRequest.DONE) {
+             //take some action
+             
+             if(request.status===200) { 
+                 //capturea alist of names and render it as a list
+                  var names= ['name1','name2','name3'];
+                  var list='';
+                  for(var i=0; i< names.length; i++){
+                 list += '<li>' + names[i] + '</li>';
+                }
      var ul = document.getElementById('namelist');
      ul.innerHTML= list;
+             }
+         }
+         //not done yet
+     };
+     //make the request
+     request.open('GET','http://harshasuryavamshi.imad.hasura-app.io/submit-name?name=' + name,true);
+     request.send(null);
+    
  };
      
