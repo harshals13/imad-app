45467033bc644571068b4ff81400f68f94647a94
Harsha Suryavamshi <harshasuryavamshi@gmail.com>
2017-08-22 07:25:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@
      
      //capture the response and store it in a variable
      
-     request.onreadystatechange = function () {
+     request.onreadystatechange = function (){
          if(request.readyState===XMLHttpRequest.DONE) {
              //take some action
              
@@ -23,3 +23,21 @@
      request.open('GET','http://harshasuryavamshi.imad.hasura-app.io/',true);
      request.send(null);
  };
+ 
+ //submit name
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;
+ var submit = document.getElementById('submit_btn');
+ submit.onclick = function(){
+     //make a request to the server and send the name
+     
+     //capturea alist of names and render it as a list
+     var names= ['name1','name2','name3'];
+     var list='';
+     for(var i=0; i< names.length; i++){
+         list+= '<li>' + names[i] + '</li>';
+     }
+     var ul = document.getElementById('namelist');
+     ul.innerHTML= list;
+ };
+     
