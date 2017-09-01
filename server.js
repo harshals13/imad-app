5ea1bdf674b6f9dfa4ae744998ad1a7894db9c04
Harsha Suryavamshi <harshasuryavamshi@gmail.com>
2017-09-01 13:27:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get('/articles/:articleName',function(req,res){
    //articles[articleName]== {} content object for article one
     
     
-    pool.query("SELECT * FROM article WHERE title =" + req.params.articleName, function (err,result){
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName, function (err,result){
         
         if(err){
             res.send(500).send(err.toString());
