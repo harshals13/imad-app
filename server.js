5730d41e7f4ccbde0853ad057e86a981d9aa6c98
Harsha Suryavamshi <harshasuryavamshi@gmail.com>
2017-09-03 19:54:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,13 +115,13 @@ app.post('/login', function(req,res){
        
         if(err){
             res.status(500).send(err.toString());
-        } else{
+        } else {
             if(result.rows.length === 0){
                 res.send(403).send('username/password is invalid');
             }else{
              //match the password
              var dbString = result.rows[0].password;
-             var salt = dbSstring.split('$')[2];
+             var salt = dbString.split('$')[2];
              var hashedPassword = hash(password, salt); //Creating a hash based on the passsword submitted and the original salt
              if(hashedPassword === dbString){
                  res.send("Credential correct!");
